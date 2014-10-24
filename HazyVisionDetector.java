f4fe488a197fc98fc7b358d415e05f397285e08a
James <james.asghar@gmail.com>
2014-10-23 19:36:18 -0500

    Finished Main Code, need to test now
    Got rid of of HazyConnect and HazyDetector and condensed them into
    HazyVisionDetector

---
+++ HazyVisionDetector.java
@@ -0,0 +1,57 @@
+public class HazyVisionDetector {
+
+  Vector byteVector = new Vector();
+  int[] byteInt = new int[5];
+  SocketConnection connection;
+  int listenPort;
+  String hazyOutput;
+
+  public HazyVisionDetector(SocketConnection c, int lPort) {
+    connection = c;
+    listenPort = lPort;
+  }
+
+  public String hazyConnect() {
+    ServerSocketConnection s = null;
+    try {
+      s = (ServerSocketConnection) Connector.open("serversocket://:" + listenport);
+      SocketConnection connection = (SocketConnection) s.acceptAndOpen();
+      for(i = 0; i < byteInt.length; i++) {
+        hazyOutput += (String) this.hazyParse(i);
+      }
+    }
+    catch(IOException ex) {
+        System.out.println("There was an error.");
+    }
+    return hazyOutput;
+  }
+
+  private int hazyParse(int iterator) {
+
+    int[] bStream = this.hazyConnect();
+    int bIterated = bStream[iterator];
+    return bIterated;
+  }
+
+  private int[] hazyRead() {
+    try {
+        InputStream is = connection.openInputStream();
+        byte[] b = new byte[1024];
+        if(is.available() > 0) {
+            int read = is.read(b);
+            for(int i = 0; i < read; i++) {
+                byte reading = b[i];
+                int byteRead = reading;
+                if(byteRead != 0) {
+                  byteStream.add(byteRead);
+                }
+            }
+        }
+        is.close();
+        connection.close();
+    }
+    catch (IOException e) {
+    }
+    return byteInt = byteVector.toArray();
+  }
+}
