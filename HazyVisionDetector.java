c1435eb5acb45b914733c5cab36519bf62b4d747
James <james.asghar@gmail.com>
2014-10-28 01:15:51 -0500

    Added some comments
    Added comments for clarity and fixed some issues dealing format

--- HazyVisionDetector.java
+++ HazyVisionDetector.java
@@ -24,37 +24,48 @@ public class HazyVisionDetector {
         try {
             s = (ServerSocketConnection) Connector.open("serversocket://:" + listenPort);
             System.out.println("Let's connect!");
-        } catch(IOException ex) {
+        } 
+		catch(IOException ex) {
+			System.out.println("Could not connect or port is already open");
         }
-        // Connect
+        // Attempts to connect
         try {
             connection = (SocketConnection) s.acceptAndOpen();
-        } catch(IOException e) {
+        } 
+		catch(IOException e) {
         }
-        
-        // Everthing else
+        // Assigns an int to the output of HazyRead
         hazyOutput = this.hazyRead();
-        
+        // Returns the assigned int
         return hazyOutput;
     }
 
     private int hazyRead() {
-        int byteRead = 0;
-        try {
+		// Sets the integer that will be returned to 0
+		int byteRead = 0;
+		byte[] b = new byte[1024];
+        // Attempts to receive input from socket
+		try {
             InputStream is = connection.openInputStream();
-            byte[] b = new byte[1024];
+			// If there are bytes in the stream then
+			// the byte array is assigned to an int
             if(is.available() > 0) {
                 int read = is.read(b);
+				// Iterates through each byte in the
+				// byte array and assigns it 
                 for(int i = 0; i < read; i++) {
                     byte reading = b[i];
                     byteRead = reading;
                 }
             }
+			// Closes both the input stream and socket
             is.close();
             connection.close();
-        } catch (IOException e) {
-            System.out.println("OOOOPSIE Error:"+e);
         }
+		catch (IOException e) {
+            System.out.println("OOOOPSIE Error: "+e);
+        }
+		// Returns each assigned int
         return byteRead;
     }
 }
